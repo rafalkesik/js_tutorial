26c6ccf2b9a1006d1fb75dc73e298ebbd5ebf996
Rafał Kęsik <kesik.rafal@yahoo.com>
2024-07-06 21:12:39 +0200

    Add another function creating objects that uses Phrase object as a prototype

--- palindrome.js
+++ palindrome.js
@@ -7,9 +7,30 @@ function reverse(string) {
 function Phrase(content) {
     this.content = content;
 
+    this.processor = function(string) {
+        return string.toLowerCase();
+    }
+
+    // Returns content processed for palindrome testing.
+    this.processedContent = function processedContent() {
+        return this.processor(this.content);
+    }
+
     // Returns true for a palindrome, false otherwise.
     this.palindrome = function palindrome() {
-        let processedContent = this.content.toLowerCase();
-        return processedContent === reverse(processedContent);
+        return this.processedContent() === reverse(this.processedContent());
     }
 }
+
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+    this.content = content;
+    this.translation = translation;
+
+    // Returns tranlation processed for palindrome testing.
+    this.processedContent = function processedContent() {
+        return this.processor(this.translation);
+    }
+}
+
+TranslatedPhrase.prototype = new Phrase();

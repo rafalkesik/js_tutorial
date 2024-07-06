a7f44dc81beb1127ecb9d3feef156bf6d14690e6
Rafał Kęsik <kesik.rafal@yahoo.com>
2024-07-06 21:42:55 +0200

    clear up code for lesson 8

--- palindrome.js
+++ palindrome.js
@@ -1,36 +1,20 @@
-// Reverses a string.
-function reverse(string) {
-    return Array.from(string).reverse().join("");
-}
-
-// Defines a Phrase object.
-function Phrase(content) {
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+    return Array.from(this).reverse().join("");
+  }
+  
+  // Defines a Phrase object.
+  function Phrase(content) {
     this.content = content;
-
-    this.processor = function(string) {
-        return string.toLowerCase();
-    }
-
+  
     // Returns content processed for palindrome testing.
     this.processedContent = function processedContent() {
-        return this.processor(this.content);
+      return this.content.toLowerCase();
     }
-
-    // Returns true for a palindrome, false otherwise.
+  
+    // Returns true if the phrase is a palindrome, false otherwise.
     this.palindrome = function palindrome() {
-        return this.processedContent() === reverse(this.processedContent());
+      return this.processedContent() === this.processedContent().reverse();
     }
-}
-
-// Defines a TranslatedPhrase object.
-function TranslatedPhrase(content, translation) {
-    this.content = content;
-    this.translation = translation;
-
-    // Returns tranlation processed for palindrome testing.
-    this.processedContent = function processedContent() {
-        return this.processor(this.translation);
-    }
-}
-
-TranslatedPhrase.prototype = new Phrase();
+  }
+  

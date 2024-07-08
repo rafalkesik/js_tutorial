72c44d9e97266ca9ce45770cbcae48ef1d36b3ad
Rafał Kęsik <kesik.rafal@yahoo.com>
2024-07-08 18:15:58 +0200

    Add a button which prompts for a word for palindrome test

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 let Phrase = require("rafalkesik-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palindromeTester() {
+    let string = prompt("Please enter a string for palindrome testing:");
+    let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-    alert(`${phrase.content} is a palindrome!`);
-} else {
-    alert(`${phrase.content} is not a palindrome.`);
+    if (phrase.palindrome()) {
+        alert(`${phrase.content} is a palindrome!`);
+    } else {
+        alert(`${phrase.content} is not a palindrome.`);
+    }
 }
 
-// alert(new Phrase("Madam, I'm Adam.").palindrome());
+document.addEventListener("DOMContentLoaded", function() {
+    let button = document.querySelector("#palindromeTester");
+    button.addEventListener("click", function() {
+        palindromeTester();
+    });
+});

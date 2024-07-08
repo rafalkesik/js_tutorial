44adc5f6a0de6ffb0bb7a178d509e6cea137dbf4
Rafał Kęsik <kesik.rafal@yahoo.com>
2024-07-08 19:17:02 +0200

    Allow the form to show the result without submitting data to database & & update rafalkesik-palindrome npm module to bugfix empty input showing true for palindrome test

--- bundle.js
+++ bundle.js
@@ -1,9 +1,10 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let Phrase = require("rafalkesik-palindrome");
 
-function palindromeTester() {
-    let string = prompt("Please enter a string for palindrome testing:");
-    let phrase = new Phrase(string);
+function palindromeTester(event) {
+    event.preventDefault();
+
+    let phrase = new Phrase(event.target.phrase.value);
     let palindromeResult = document.querySelector("#palindromeResult");
 
     if (phrase.palindrome()) {
@@ -14,14 +15,14 @@ function palindromeTester() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    let button = document.querySelector("#palindromeTester");
-    button.addEventListener("click", function() {
-        palindromeTester();
+    let tester = document.querySelector("#palindromeTester");
+    tester.addEventListener("submit", function(event) {
+        palindromeTester(event);
     });
 });
 
 },{"rafalkesik-palindrome":2}],2:[function(require,module,exports){
-// code ready for 8.5.1
+// code ready for lesson 9.
 
 module.exports = Phrase;
 
@@ -38,23 +39,22 @@ String.prototype.reverse = function() {
     this.processedContent = function processedContent() {
       return this.letters().toLowerCase();
     }
-    
-    // Returns the letters in the content.
-    this.letters = function letters() {
-      return this.content;  // stub return value
-    }
 
     // Returns the letters in the content.
     // For example:
     //    new Phrase("Hello, world!").letters() === "Helloworld"
     this.letters = function letters() {
-      // return Array.from(this.content).filter(a => a.match(/[a-z]/i)).join("")
-      return (this.content.match(/[a-z]/gi) || []).join("");
+      const lettersRegEx = /[a-z]/gi;
+      return (this.content.match(lettersRegEx) || []).join("");
     }
 
     // Returns true if the phrase is a palindrome, false otherwise.
     this.palindrome = function palindrome() {
-      return this.processedContent() === this.processedContent().reverse();
+      if (this.processedContent()) {
+        return this.processedContent() === this.processedContent().reverse();
+      } else {
+        return false;
+      }
     }
 
   }
